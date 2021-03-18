00b1921383e19105c546acbfeb1e33d2b18b5e5e
alexraskin <alexraskin@fastmail.fm>
2021-03-17 22:21:24 -0700

    updates

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/alexraskin/improved-lamp/main/src/app.py)
+
+# Username Generater Streamlit App
--- src/app.py
+++ src/app.py
@@ -1,22 +1,27 @@
+import emoji
+import random
 import streamlit as st
 
 from random_username.generate import generate_username
 
-st.title('Python Username Generater')
+st.set_page_config(
+    page_title="Python Username Generater",
+    layout="wide")
 
-st.markdown("![Alt Text](https://media.giphy.com/media/okLCopqw6ElCDnIhuS/giphy.gif)")
+emoji_list = [':smile:', ':flushed:', ':scream:', ':bowtie:', ':cold_sweat:', ':fire:', ':fist:', ':collision:', ':laughing:']
 
-user_number = st.number_input(label='How many usernames would you like to generate?', min_value=1, max_value=5)
+st.sidebar.text('Python Username Generater')
 
-if user_number == 1:
-    st.write(generate_username(1))
-elif user_number == 2:
-    st.write(generate_username(2))
-elif user_number == 3:
-    st.write(generate_username(3))
-elif user_number == 4:
-    st.write(generate_username(4))
-elif user_number == 5:
-    st.write(generate_username(5))
-else:
-    st.stop()
+col1, col2 = st.beta_columns([2, 1])
+
+with col1:
+    st.subheader('How many usernames would you like to generate?')
+    # user_number = st.number_input(label='Amount', min_value=1, max_value=5)
+    button = st.button(label=emoji.emojize('Generate some usernames :rocket:'))
+
+with col2:
+    st.subheader('Your generated usernames')
+    if button:
+        user_name = generate_username(1)
+        for i in user_name:
+            st.write(i, emoji.emojize(random.choice(emoji_list)))
